8866c94399eb6342ad963fbc7b21bfbdf2721ae7
kochan17 <i7811832616@gmail.com>
2026-01-30 14:40:48 +0900

    管理画面の基盤をNext.jsからVite/Reactへ移行：構成の刷新とディレクトリ名の変更

--- admin-page/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import VideoEditor from './components/VideoEditor';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-zinc-50">
-      <VideoEditor />
-    </main>
-  );
-}
